0c914fe6d2df587b3eb6d20bf76d714b2345c7b2
JorNeu <jorgeeneuendorf@gmail.com>
2024-05-28 01:49:36 -0300

    editando algunas entradas y variables

--- apidb.js
+++ apidb.js
@@ -1,54 +1,54 @@
 const apiKey = '44f87301db7c7058bdddf60e649353a1';
 document.addEventListener('DOMContentLoaded', () => {
-    fetchTrendingMovies();
-    fetchTopRatedMovies();
+    fetchPelisDelMomento();
+    fetchMejoresPelis();
 });
 
-function fetchTrendingMovies() {
+function fetchPelisDelMomento() {
     fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}&language=es`)
         .then(response => response.json())
         .then(data => {
-            const movies = data.results;
-            const trendingMoviesContainer = document.getElementById('trending-movies');
-            trendingMoviesContainer.innerHTML = '';
-            movies.forEach(movie => {
-                const movieElement = createMovieElement(movie);
-                trendingMoviesContainer.appendChild(movieElement);
+            const pelis = data.results;
+            const PelisDelMomentoContainer = document.getElementById('trending-pelis');
+            PelisDelMomentoContainer.innerHTML = '';
+            pelis.forEach(pelis => {
+                const PeliElement = crearPeli(pelis);
+                PelisDelMomentoContainer.appendChild(PeliElement);
             });
         })
-        .catch(error => console.error('Error fetching trending movies:', error));
+        .catch(error => console.error('Error fetching trending pelis:', error));
 }
 
-function fetchTopRatedMovies() {
+function fetchMejoresPelis() {
     fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=es`)
         .then(response => response.json())
         .then(data => {
-            const movies = data.results;
-            const topRatedMoviesContainer = document.getElementById('top-rated-movies');
-            topRatedMoviesContainer.innerHTML = '';
-            movies.forEach(movie => {
-                const movieElement = createMovieElement(movie);
-                topRatedMoviesContainer.appendChild(movieElement);
+            const pelis = data.results;
+            const MejoresPelisContainer = document.getElementById('top-rated-pelis');
+            MejoresPelisContainer.innerHTML = '';
+            pelis.forEach(pelis => {
+                const PeliElement = crearPeli(pelis);
+                MejoresPelisContainer.appendChild(PeliElement);
             });
         })
-        .catch(error => console.error('Error fetching top-rated movies:', error));
+        .catch(error => console.error('Error fetching top-rated pelis:', error));
 }
 
-function createMovieElement(movie) {
-    const movieContainer = document.createElement('div');
-    movieContainer.className = 'col-lg-3 col-md-4 col-sm-6 movie-container';
+function crearPeli(pelis) {
+    const pelisContainer = document.createElement('div');
+    pelisContainer.className = 'col-lg-3 col-md-4 col-sm-6 pelis-container';
     
-    const movieImage = document.createElement('img');
-    movieImage.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    movieImage.alt = movie.title;
-    movieImage.className = 'img-fluid movie';
+    const pelinsImagen = document.createElement('img');
+    pelinsImagen.src = `https://image.tmdb.org/t/p/w500${pelis.poster_path}`;
+    pelinsImagen.alt = pelis.title;
+    pelinsImagen.className = 'img-fluid pelis';
 
-    const movieTitle = document.createElement('div');
-    movieTitle.className = 'nombre-peli';
-    movieTitle.innerText = movie.title;
+    const pelisTitulo = document.createElement('div');
+    pelisTitulo.className = 'nombre-peli';
+    pelisTitulo.innerText = pelis.title;
 
-    movieContainer.appendChild(movieImage);
-    movieContainer.appendChild(movieTitle);
+    pelisContainer.appendChild(pelinsImagen);
+    pelisContainer.appendChild(pelisTitulo);
     
-    return movieContainer;
+    return pelisContainer;
 }
